91ba5787aaf82068abadfa7d56e9cb50522b4cd5
Evan Maloney <emaloney@gilt.com>
2015-07-01 02:06:14 -0400

    a script to automate building certain documents
    files like INTEGRATION.md are being turned into Boilerplate template files, so they can be rebuilt for each repo
    
    previously, we'd have to copy & paste changes by hand, which was difficult, because each repo has slightly different versions of these files based on the structure of the project

---
+++ Deployment/bin/docbuilder.sh
@@ -0,0 +1,188 @@
+#!/bin/bash
+
+SCRIPT_NAME=`basename $0`
+SCRIPT_DIR=`dirname "$PWD/$0"`
+
+cd "$SCRIPT_DIR/.."
+
+#
+# parse the command-line arguments
+#
+FILE_LIST=()
+REPO_LIST=()
+while [[ $1 ]]; do
+	case $1 in
+	--help|-h|-\?)
+		SHOW_HELP=1
+		;;
+	
+ 	--file|-f)
+ 		while [[ $2 ]]; do
+ 			case $2 in
+ 			-*)
+ 				break
+ 				;;
+ 				
+ 			*)
+				FILE_LIST+=($2)
+		 		shift
+				;;	
+ 			esac
+ 		done
+ 		;;
+	
+ 	--repo|-r)
+ 		REPOS_SPECIFIED=1
+ 		while [[ $2 ]]; do
+ 			case $2 in
+ 			-*)
+ 				break
+ 				;;
+ 				
+ 			*)
+				REPO_LIST+=($2)
+				shift
+				;;	
+ 			esac
+ 		done
+ 		;;
+ 		
+ 	--no-commit)
+ 		NO_COMMIT=1
+ 		;;
+	
+	*)
+		exitWithErrorSuggestHelp "Unrecognized argument: $1"
+		;;
+	esac
+	shift
+done
+
+showHelp()
+{
+	echo "$SCRIPT_NAME"
+	echo
+	printf "\tRegenerates one or more boilerplate files for one or more\n"
+	printf "\tof the Cleanroom Project code repos.\n"
+	echo
+	echo "Usage:"
+	echo
+	printf "\t$SCRIPT_NAME -f <file> [<file> [<file> [...]]]\n"
+	echo
+	echo "Where:"
+	echo
+	printf "\t<file> is the relative path of a file to be generated\n"
+	printf "\twithin the Cleanroom Project repo(s)\n"
+	echo
+	echo "Additional arguments:"
+	echo
+# 	printf "\t<file> is the relative path of a file to be generated\n"
+# 	printf "\twithin the Cleanroom Project repo(s)\n"
+	echo
+	echo "Help"
+	echo
+	printf "\tThis documentation is displayed when supplying the --help (or\n"
+	printf "\t-h or -?) argument.\n"
+	echo
+	printf "\tNote that when this script displays help documentation, all other\n"
+	printf "\tcommand line arguments are ignored and no other actions are performed.\n"
+	echo
+}
+
+printError()
+{
+	echo "error: $1"
+	echo
+	if [[ ! -z $2 ]]; then
+		printf "  $2\n\n"
+	fi
+}
+
+exitWithError()
+{
+	printError "$1" "$2"
+	exit 1
+}
+
+exitWithErrorSuggestHelp()
+{
+	printError "$1" "$2"
+	printf "  To display help, run:\n\n\t$0 --help\n"
+	exit 1
+}
+
+isNotRepo()
+{
+	REPO_DIR="../../$1"
+	if [[ ! -d "$REPO_DIR" ]]; then
+		echo 1
+	else
+		pushd "$REPO_DIR" > /dev/null
+		git status 2&> /dev/null
+		RESULT=$?
+		popd > /dev/null
+		echo $RESULT
+	fi
+}
+
+expectRepo()
+{
+	if [[ $(isNotRepo "$1") != 0 ]]; then
+		REPO_PARENT="`cd ../..; pwd`"
+		echo "error: Expected $1 (within the directory $REPO_PARENT) to be a git repo"
+		exit 1
+	fi
+}
+
+if [[ $SHOW_HELP ]]; then
+	showHelp
+	exit 1
+fi
+
+#
+# if no repos were specified, use everything we have data for
+#
+if [[ ! $REPOS_SPECIFIED ]]; then
+	for f in repos/*.xml; do
+		REPO_LIST+=(`echo $f | sed "s/^repos\///" | sed "s/.xml$//"`)
+	done
+fi
+
+if [[ ${#REPO_LIST[@]} < 1 ]]; then
+	exitWithErrorSuggestHelp "At least one repo must be specified"
+fi
+
+#
+# make sure everything we were handed looks like a real repo
+#
+for r in ${REPO_LIST[@]}; do
+	expectRepo "$r"
+done
+
+#
+# make sure boilerplate exists for each file specified
+#
+for f in ${FILE_LIST[@]}; do
+	BOILERPLATE_FILE="boilerplate/$f.boilerplate"
+	if [[ ! -f "$BOILERPLATE_FILE" ]]; then
+		echo "error: Expected to find boilerplate file at $BOILERPLATE_FILE (within the directory $PWD)"
+		exit 1
+	fi
+done
+
+#
+# process each file for each repo
+#
+for f in ${FILE_LIST[@]}; do
+	BOILERPLATE_FILE="boilerplate/$f.boilerplate"
+
+	echo "Generating $f..."
+	for r in ${REPO_LIST[@]}; do
+		printf "    ...for the $r repo"
+		./bin/boiler -t "$BOILERPLATE_FILE" -d repos/${r}.xml -m include/repos.xml -o "../../$r/$f"
+		if [[ "$?" != 0 ]]; then
+			exit 3
+		fi
+		printf " (done!)\n"
+	done
+done
