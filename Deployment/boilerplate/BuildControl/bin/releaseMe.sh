8022043398b4de6bbe653addf7f6f3b37e731339
Evan Maloney <emaloney@gilt.com>
2015-09-15 16:15:21 -0400

    build all & run all unit tests before release

--- Deployment/boilerplate/BuildControl/bin/releaseMe.sh
+++ Deployment/boilerplate/BuildControl/bin/releaseMe.sh
@@ -6,8 +6,8 @@
 # by emaloney, 7 June 2015
 #
 
-SCRIPT_NAME=`basename $0`
-SCRIPT_DIR=`dirname "$PWD/$0"`
+SCRIPT_NAME=$(basename "$0")
+SCRIPT_DIR=$(cd $PWD ; cd `dirname "$0"` ; echo $PWD)
 
 showHelp()
 {
@@ -442,12 +442,19 @@ if [[ ! -x "$XCODEBUILD" ]]; then
 fi
 
 #
-# build each scheme to try
+# build each scheme we can find
 #
 xcodebuild -list | grep "\s${REPO_NAME}" | grep -v Tests | sort | uniq | sed "s/^[ \t]*//" | while read SCHEME
 do
-	updateStatus "Building $SCHEME..."
+	updateStatus "Building: $SCHEME..."
 	executeCommand "$XCODEBUILD -project ${REPO_NAME}.xcodeproj -scheme \"$SCHEME\" -configuration Release clean build"
+done
+
+xcodebuild -list | grep "\s${REPO_NAME}" | grep UnitTests | sort | uniq | sed "s/^[ \t]*//" | while read TARGET
+do
+	SCHEME=$(echo "$TARGET" | sed sqUnitTestsqq)
+	updateStatus "Executing unit tests: $TARGET for $SCHEME..."
+	executeCommand "$XCODEBUILD -project ${REPO_NAME}.xcodeproj -scheme \"$SCHEME\" -configuration Release clean test"
 done
 
 updateStatus "Adjusting version numbers"
@@ -476,4 +483,3 @@ if [[ $PUSH_WHEN_DONE ]]; then
 	executeCommand "git push"
 	executeCommand "git push --tags"
 fi
-
